154ccfcc24cddb7e2480e49ab67cd5224aebc4ef
Zed(Zijun) Chen <zed@zijun.dev>
2023-08-18 21:59:40 +0800

    _

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:project_dv/gps.dart';
+import 'package:project_dv/map.dart';
 import 'ffi.dart' if (dart.library.html) 'ffi_web.dart';
 
 void main() async {
@@ -73,84 +74,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: const Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text("You're running on"),
-            // To render the results of a Future, a FutureBuilder is used which
-            // turns a Future into an AsyncSnapshot, which can be used to
-            // extract the error state, the loading state and the data if
-            // available.
-            //
-            // Here, the generic type that the FutureBuilder manages is
-            // explicitly named, because if omitted the snapshot will have the
-            // type of AsyncSnapshot<Object?>.
-            FutureBuilder<List<dynamic>>(
-              // We await two unrelated futures here, so the type has to be
-              // List<dynamic>.
-              future: Future.wait([platform, isRelease]),
-              builder: (context, snap) {
-                final style = Theme.of(context).textTheme.headlineMedium;
-                if (snap.error != null) {
-                  // An error has been encountered, so give an appropriate response and
-                  // pass the error details to an unobstructive tooltip.
-                  debugPrint(snap.error.toString());
-                  return Tooltip(
-                    message: snap.error.toString(),
-                    child: Text('Unknown OS', style: style),
-                  );
-                }
-
-                // Guard return here, the data is not ready yet.
-                final data = snap.data;
-                if (data == null) return const CircularProgressIndicator();
-
-                // Finally, retrieve the data expected in the same order provided
-                // to the FutureBuilder.future.
-                final Platform platform = data[0];
-                final release = data[1] ? 'Release' : 'Debug';
-                final text = const {
-                      Platform.Android: 'Android',
-                      Platform.Ios: 'iOS',
-                      Platform.MacApple: 'MacOS with Apple Silicon',
-                      Platform.MacIntel: 'MacOS',
-                      Platform.Windows: 'Windows',
-                      Platform.Unix: 'Unix',
-                      Platform.Wasm: 'the Web',
-                    }[platform] ??
-                    'Unknown OS';
-                return Text('$text ($release)', style: style);
-              },
+            GPS(),
+            Expanded(
+              child: MapUiBody(),
             ),
-            const GPS()
           ],
         ),
       ),
---
+++ app/lib/map.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:maplibre_gl/mapbox_gl.dart';
+import 'dart:ui';
+
+class MapUiBody extends StatefulWidget {
+  const MapUiBody();
+
+  @override
+  State<StatefulWidget> createState() => MapUiBodyState();
+}
+
+class MapUiBodyState extends State<MapUiBody> {
+  MapUiBodyState();
+
+  static const CameraPosition _kInitialPosition = CameraPosition(
+    target: LatLng(-33.852, 151.211),
+    zoom: 11.0,
+  );
+
+  MaplibreMapController? mapController;
+  ByteData? image;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<ByteData?> drawImage() async {
+    final recorder = PictureRecorder();
+    final canvas = Canvas(recorder,
+        Rect.fromPoints(const Offset(0.0, 0.0), const Offset(200.0, 200.0)));
+
+    final stroke = Paint()
+      ..color = const Color.fromARGB(128, 0, 0, 0)
+      ..style = PaintingStyle.fill;
+
+    canvas.drawRect(const Rect.fromLTWH(0.0, 0.0, 200.0, 200.0), stroke);
+
+    final picture = recorder.endRecording();
+    final img = await picture.toImage(200, 200);
+    return await img.toByteData(format: ImageByteFormat.png);
+  }
+
+  void _onMapCreated(MaplibreMapController controller) async {
+    controller.addListener(_onMapChanged);
+    mapController = controller;
+  }
+
+  _onStyleLoadedCallback() async {
+    var controller = mapController;
+    if (controller == null) return;
+    final visiableRegion = await controller.getVisibleRegion();
+    image = await drawImage();
+    final topLeft = LatLng(
+        visiableRegion.northeast.latitude, visiableRegion.southwest.longitude);
+    final topRight = visiableRegion.northeast;
+    final bottomRight = LatLng(
+        visiableRegion.southwest.latitude, visiableRegion.northeast.longitude);
+    final bottomLeft = visiableRegion.southwest;
+    final coordinates = LatLngQuad(
+        topLeft: topLeft,
+        topRight: topRight,
+        bottomRight: bottomRight,
+        bottomLeft: bottomLeft);
+    await controller.addImageSource(
+        "main-image-source", image!.buffer.asUint8List(), coordinates);
+    await controller.addImageLayer("main-image-layer", "main-image-source");
+  }
+
+  void _onMapChanged() async {
+    final position = mapController?.cameraPosition;
+    if (position == null) return;
+    // final isMoving = mapController!.isCameraMoving;
+    final visiableRegion = await mapController!.getVisibleRegion();
+    final zoom = position.zoom;
+    final topLeft = LatLng(
+        visiableRegion.northeast.latitude, visiableRegion.southwest.longitude);
+    final topRight = visiableRegion.northeast;
+    final bottomRight = LatLng(
+        visiableRegion.southwest.latitude, visiableRegion.northeast.longitude);
+    final bottomLeft = visiableRegion.southwest;
+    final coordinates = LatLngQuad(
+        topLeft: topLeft,
+        topRight: topRight,
+        bottomRight: bottomRight,
+        bottomLeft: bottomLeft);
+    await mapController?.updateImageSource(
+        "main-image-source", image!.buffer.asUint8List(), coordinates);
+  }
+
+  @override
+  void dispose() {
+    mapController?.removeListener(_onMapChanged);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final MaplibreMap maplibreMap = MaplibreMap(
+      onMapCreated: _onMapCreated,
+      onStyleLoadedCallback: _onStyleLoadedCallback,
+      initialCameraPosition: _kInitialPosition,
+      trackCameraPosition: true,
+      myLocationRenderMode: MyLocationRenderMode.NORMAL,
+    );
+
+    return maplibreMap;
+  }
+}
