5c1ad926524637f5eb406f5d8442fc73a32f9247
Zed(Zijun) Chen <zed@zijun.dev>
2023-08-26 00:07:32 +0800

    Add testing for the rust part (#5)
    All core logic will be implemented in rust, and having a way to test the
    core logic easily can be extremely helpful. e.g. most work can be done
    without setting up the flutter part.

--- app/native/src/lib.rs
+++ app/native/src/lib.rs
@@ -1,7 +1,10 @@
-#[macro_use] extern crate log;
-#[macro_use] extern crate anyhow;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate anyhow;
 
 mod api;
 mod bridge_generated;
 
+pub mod map_renderer;
 mod storage;
---
+++ app/native/tests/map_renderer.rs
@@ -0,0 +1,35 @@
+use std::{fs::File, io::Write};
+
+use native::map_renderer::*;
+
+use hex::ToHex;
+use sha1::{Digest, Sha1};
+
+#[test]
+fn basic() {
+    let map_renderer = MapRenderer::new();
+    let render_result = map_renderer.render_map_overlay(
+        11.0,
+        151.1435370795134,
+        -33.793291910360125,
+        151.2783692841415,
+        -33.943600147192235,
+    );
+    assert_eq!(render_result.left, 150.99609375);
+    assert_eq!(render_result.top, -33.72433966174759);
+    assert_eq!(render_result.right, 151.34765625);
+    assert_eq!(render_result.bottom, -34.016241889667015);
+
+    // capture image changes
+    let mut hasher = Sha1::new();
+    hasher.update(&render_result.data.0);
+    let result = hasher.finalize();
+    assert_eq!(
+        result.encode_hex::<String>(),
+        "3f97a9f76b3dd80bbac9d0b0b7cb30529afbe827"
+    );
+
+    // for human inspection
+    let mut f = File::create("./tests/for_inspection/map_renderer_basic.png").unwrap();
+    f.write_all(&render_result.data.0).unwrap();
+}
