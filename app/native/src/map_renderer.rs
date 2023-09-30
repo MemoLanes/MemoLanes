8623cd634e2c2d54f504f5c94218caf4a91979b0
Zed(Zijun) Chen <zed@zijun.dev>
2023-09-30 23:04:57 +0800

    Renderer improvements (#9)
    This PR includes:
    - Use the proper upstream for fultter-maplibre-gl, our image source PR
    is merged: https://github.com/maplibre/flutter-maplibre-gl/pull/271
    - Only trigger a rerender when needed.
    
    However, there are bugs when antimeridian is in the view, and it require
    some none trivial changes in the upstream lib:
    https://github.com/maplibre/maplibre-native/issues/1681

--- app/native/src/map_renderer.rs
+++ app/native/src/map_renderer.rs
@@ -11,16 +11,16 @@ pub struct RenderResult {
 }
 
 // https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
-fn lng_lat_to_tile_xy(lng: f64, lat: f64, zoom: f32) -> (i32, i32) {
-    let n = f64::powi(2.0, zoom as i32);
+fn lng_lat_to_tile_xy(lng: f64, lat: f64, zoom: i32) -> (i32, i32) {
+    let n = f64::powi(2.0, zoom);
     let lat_rad = (lat / 180.0) * std::f64::consts::PI;
     let x = ((lng + 180.0) / 360.0) * n;
     let y = (1.0 - ((lat_rad.tan() + 1.0 / lat_rad.cos()).ln() / std::f64::consts::PI)) / 2.0 * n;
     (x.floor() as i32, y.floor() as i32)
 }
 
-fn tile_xy_to_lng_lat(x: i32, y: i32, zoom: f32) -> (f64, f64) {
-    let n = f64::powi(2.0, zoom as i32);
+fn tile_xy_to_lng_lat(x: i32, y: i32, zoom: i32) -> (f64, f64) {
+    let n = f64::powi(2.0, zoom);
     let lng = (x as f64 / n) * 360.0 - 180.0;
     let lat = (f64::atan(f64::sinh(
         std::f64::consts::PI * (1.0 - (2.0 * y as f64) / n),
@@ -29,24 +29,27 @@ fn tile_xy_to_lng_lat(x: i32, y: i32, zoom: f32) -> (f64, f64) {
     (lng, lat)
 }
 
-pub struct MapRenderer {}
+#[derive(PartialEq, Eq)]
+struct RenderArea {
+    zoom: i32,
+    left_idx: i32,
+    top_idx: i32,
+    right_idx: i32,
+    bottom_idx: i32,
+}
+
+pub struct MapRenderer {
+    current_render_area: Option<RenderArea>,
+}
 
 impl MapRenderer {
     pub fn new() -> Self {
-        MapRenderer {}
+        MapRenderer {
+            current_render_area: None,
+        }
     }
 
-    // TODO: keep the info (tile_idx etc) about the last overlay somewhere and skip rendering
-    // if nothing changes (e.g. return `None` as `RenderResult`).
-    pub fn render_map_overlay(
-        &self,
-        // map view area (coordinates are in lat or lng)
-        zoom: f32,
-        left: f64,
-        top: f64,
-        right: f64,
-        bottom: f64,
-    ) -> RenderResult {
+    fn render_map_overlay(&self, render_area: &RenderArea) -> RenderResult {
         // TODO: Change render backend. Right now we are using `tiny-skia`,
         // it should work just fine and we don't really need fancy features.
         // However, it is mostly a research project and does not feel like production ready,
@@ -55,12 +58,13 @@ impl MapRenderer {
         // now is that it is pure rust, so we don't need to think about how to build depenceies
         // for various platform.
 
-        let (left_idx, top_idx) = lng_lat_to_tile_xy(left, top, zoom);
-        let (right_idx, bottom_idx) = lng_lat_to_tile_xy(right, bottom, zoom);
-
         const TILE_SIZE: u32 = 128;
-        let width_by_tile: u32 = (right_idx - left_idx + 1).try_into().unwrap();
-        let height_by_tile: u32 = (bottom_idx - top_idx + 1).try_into().unwrap();
+        let width_by_tile: u32 = (render_area.right_idx - render_area.left_idx + 1)
+            .try_into()
+            .unwrap();
+        let height_by_tile: u32 = (render_area.bottom_idx - render_area.top_idx + 1)
+            .try_into()
+            .unwrap();
 
         // TODO: reuse resurces?
         let mut pixmap =
@@ -96,9 +100,13 @@ impl MapRenderer {
 
         let bytes = pixmap.encode_png().unwrap();
 
-        let (overlay_left, overlay_top) = tile_xy_to_lng_lat(left_idx, top_idx, zoom);
-        let (overlay_right, overlay_bottom) =
-            tile_xy_to_lng_lat(right_idx + 1, bottom_idx + 1, zoom);
+        let (overlay_left, overlay_top) =
+            tile_xy_to_lng_lat(render_area.left_idx, render_area.top_idx, render_area.zoom);
+        let (overlay_right, overlay_bottom) = tile_xy_to_lng_lat(
+            render_area.right_idx + 1,
+            render_area.bottom_idx + 1,
+            render_area.zoom,
+        );
 
         RenderResult {
             top: overlay_top,
@@ -106,6 +114,43 @@ impl MapRenderer {
             right: overlay_right,
             bottom: overlay_bottom,
             data: ZeroCopyBuffer(bytes),
+        }
+    }
+
+    pub fn maybe_render_map_overlay(
+        &mut self,
+        // map view area (coordinates are in lat or lng)
+        zoom: f32,
+        left: f64,
+        top: f64,
+        right: f64,
+        bottom: f64,
+    ) -> Option<RenderResult> {
+        // TODO: This doesn't really work when antimeridian is involved, see
+        // the upstream issue: https://github.com/maplibre/maplibre-native/issues/1681
+        let zoom = zoom as i32;
+        let (left_idx, top_idx) = lng_lat_to_tile_xy(left, top, zoom);
+        let (mut right_idx, bottom_idx) = lng_lat_to_tile_xy(right, bottom, zoom);
+
+        if right_idx < left_idx {
+            let n = f64::powi(2.0, zoom) as i32;
+            right_idx += n;
+        }
+
+        let render_area = RenderArea {
+            zoom,
+            left_idx,
+            top_idx,
+            right_idx,
+            bottom_idx,
+        };
+        if Some(&render_area) == self.current_render_area.as_ref() {
+            // same, nothing to do
+            None
+        } else {
+            let render_result = self.render_map_overlay(&render_area);
+            self.current_render_area = Some(render_area);
+            Some(render_result)
         }
     }
 }
