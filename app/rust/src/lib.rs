cab1bab6a226cf70e621915d324d6d118516b086
Klzy <klzy@vlabpro.com>
2024-01-19 15:04:51 +0800

    fix

--- app/rust/src/lib.rs
+++ app/rust/src/lib.rs
@@ -6,7 +6,6 @@ extern crate log;
 extern crate anyhow;
 
 #[cfg(flutterbuild)]
-#[rustfmt::skip]
 mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 
 pub mod api;
