fcbbd0b86db65927653683bae87b8d8354a83dea
Klzy <klzy@vlabpro.com>
2024-01-19 15:31:02 +0800

    fix

--- app/rust/build.rs
+++ app/rust/build.rs
@@ -1,5 +1,5 @@
 use std::fs;
-
+use std::process::Command;
 // need to install:
 // - `cargo install flutter_rust_bridge_codegen`
 fn main() {
@@ -13,7 +13,25 @@ fn main() {
         .input("src/protos/archive.proto")
         .run_from_script();
 
-    if fs::metadata("src/frb_generated.rs").is_ok() {
+    
+
+    println!("cargo:rerun-if-changed=src/api/*");
+    let frb_codegen_installed = Command::new("flutter_rust_bridge_codegen")
+        .arg("--version")
+        .output()
+        .is_ok();
+    // NOTE: We skip running the frb codegen if the codegen is not installed.
+    if frb_codegen_installed && fs::metadata("src/frb_generated.rs").is_err() {
+        println!("cargo:rustc-cfg=flutterbuild");
+        let output = Command::new("flutter_rust_bridge_codegen")
+            .arg("generate")
+            .current_dir("..")
+            .output()
+            .expect("Failed to execute binary");
+        if !output.status.success() {
+            panic!("{:?}", output)
+        }
+    }else if fs::metadata("src/frb_generated.rs").is_ok() {
         println!("cargo:rustc-cfg=flutterbuild");
     }
 }
