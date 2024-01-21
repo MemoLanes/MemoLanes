e38c2683e471f0c4ab7f4866293560c544309a48
Zed(Zijun) Chen <zed@zijun.dev>
2024-01-21 17:21:32 +0800

    _

--- app/rust/build.rs
+++ app/rust/build.rs
@@ -10,10 +10,16 @@ fn main() {
         .input("src/protos/archive.proto")
         .run_from_script();
 
+    println!("cargo:rerun-if-changed=src/frb_generated.rs");
     if fs::metadata("src/frb_generated.rs").is_err() {
         fs::File::create("src/frb_generated.rs")
             .unwrap()
             .flush()
-            .expect("failed to create frb_generated.rs");
+            .expect("failed to create dummpy frb_generated.rs");
+        println!(
+            "cargo:warning=`frb_generated.rs` is not found, generating a \
+        dummpy file. If you are working on flutter, you need to run \
+        `flutter_rust_bridge_codegen generate` to get a real one."
+        );
     }
 }
