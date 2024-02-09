37bd9ccf778d6d249a9f553b50e302393f8c3af8
Zed(Zijun) Chen <zed@zijun.dev>
2024-02-09 18:22:01 +0800

    _

---
+++ app/lib/archive.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:project_dv/src/rust/api/api.dart';
+import 'package:project_dv/src/rust/journey_header.dart';
+import 'package:share_plus/share_plus.dart';
+
+class ArchiveUiBody extends StatelessWidget {
+  const ArchiveUiBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () async {
+            var result = await FilePicker.platform
+                .pickFiles(type: FileType.custom, allowedExtensions: ['zip']);
+            if (result != null) {
+              var path = result.files.single.path;
+              if (path != null) {
+                await importFowData(zipFilePath: path);
+              }
+            }
+          },
+          child: const Text("Import FoW data"),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            var tmpDir = await getTemporaryDirectory();
+            var ts = DateTime.now().millisecondsSinceEpoch;
+            var filepath = "${tmpDir.path}/${ts.toString()}.mldx";
+            await generateFullArchive(targetFilepath: filepath);
+            await Share.shareXFiles([XFile(filepath)]);
+            try {
+              var file = File(filepath);
+              await file.delete();
+            } catch (e) {
+              print(e);
+              // don't care about error
+            }
+          },
+          child: const Text("Archive All"),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            var result = await FilePicker.platform
+                // TODO: have no idea why but the commented out line doesn't work
+                // .pickFiles(type: FileType.custom, allowedExtensions: ['mldx']);
+                .pickFiles(type: FileType.any);
+            if (result != null) {
+              var path = result.files.single.path;
+              if (path != null) {
+                await recoverFromArchive(zipFilePath: path);
+              }
+            }
+          },
+          child: const Text("Reset & Recover"),
+        )
+      ],
+    );
+  }
+}
