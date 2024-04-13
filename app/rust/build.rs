22c8d863bf5bbdd36e6aac5019bbc780856ec2fa
Klzy <klzy@vlabpro.com>
2024-04-13 09:56:06 +0800

    build.rs

--- app/rust/build.rs
+++ app/rust/build.rs
@@ -35,26 +35,36 @@ fn setup_x86_64_android_workaround() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").expect("CARGO_CFG_TARGET_OS not set");
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH not set");
     if target_arch == "x86_64" && target_os == "android" {
-        let android_ndk_home = env::var("ANDROID_NDK_HOME").expect("ANDROID_NDK_HOME not set");
-        let build_os = match env::consts::OS {
-            "linux" => "linux",
-            "macos" => "darwin",
-            "windows" => "windows",
-            _ => panic!(
-                "Unsupported OS. You must use either Linux, MacOS or Windows to build the crate."
-            ),
+        match env::var("ANDROID_NDK_HOME") {
+            Ok(android_ndk_home) => {
+                let build_os = match env::consts::OS {
+                    "linux" => "linux",
+                    "macos" => "darwin",
+                    "windows" => "windows",
+                    _ => panic!(
+                        "Unsupported OS. You must use either Linux, MacOS or Windows to build the crate."
+                    ),
+                };
+                let clang_version = env::var("NDK_CLANG_VERSION")
+                    .unwrap_or_else(|_| DEFAULT_CLANG_VERSION.to_owned());
+                let linux_x86_64_lib_dir = format!(
+                    "toolchains/llvm/prebuilt/{build_os}-x86_64/lib/clang/{clang_version}/lib/linux/"
+                );
+                let linkpath = format!("{android_ndk_home}/{linux_x86_64_lib_dir}");
+                if Path::new(&linkpath).exists() {
+                    println!("cargo:rustc-link-search={android_ndk_home}/{linux_x86_64_lib_dir}");
+                    println!("cargo:rustc-link-lib=static=clang_rt.builtins-x86_64-android");
+                } else {
+                    panic!("Path {linkpath} not exists");
+                }
+            }
+            Err(_) => {
+                println!(
+                    "cargo:warning=ANDROID_NDK_HOME system environment variable not \
+                    found, if you need to compile x86_64 android version, \
+                    please set this environment variable"
+                );
+            }
         };
-        let clang_version =
-            env::var("NDK_CLANG_VERSION").unwrap_or_else(|_| DEFAULT_CLANG_VERSION.to_owned());
-        let linux_x86_64_lib_dir = format!(
-            "toolchains/llvm/prebuilt/{build_os}-x86_64/lib/clang/{clang_version}/lib/linux/"
-        );
-        let linkpath = format!("{android_ndk_home}/{linux_x86_64_lib_dir}");
-        if Path::new(&linkpath).exists() {
-            println!("cargo:rustc-link-search={android_ndk_home}/{linux_x86_64_lib_dir}");
-            println!("cargo:rustc-link-lib=static=clang_rt.builtins-x86_64-android");
-        } else {
-            panic!("Path {linkpath} not exists");
-        }
     }
 }
