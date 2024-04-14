4ff61485e1632f5d2c9a96eea5caa735446e5730
Klzy <klzy@vlabpro.com>
2024-04-14 22:30:19 +0800

    button

--- app/lib/map.dart
+++ app/lib/map.dart
@@ -3,12 +3,19 @@ import 'package:project_dv/src/rust/api/api.dart';
 import 'dart:async';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:project_dv/token.dart';
+import 'package:project_dv/utils.dart';
 
 class MapUiBody extends StatefulWidget {
   const MapUiBody({super.key});
 
   @override
   State<StatefulWidget> createState() => MapUiBodyState();
+}
+
+enum TrackingMode {
+  Display_and_tracking,
+  Display_only,
+  Off,
 }
 
 class MapUiBodyState extends State<MapUiBody> {
@@ -26,6 +33,8 @@ class MapUiBodyState extends State<MapUiBody> {
   bool layerAdded = false;
   Completer? requireRefresh = Completer();
   Timer? timer;
+  Timer? trackTimer;
+  TrackingMode trackingMode = TrackingMode.Display_and_tracking;
 
   Future<void> _doActualRefresh() async {
     var mapboxMap = this.mapboxMap;
@@ -140,22 +149,76 @@ class MapUiBodyState extends State<MapUiBody> {
     _triggerRefresh();
   }
 
+  _onMapScrollListener(ScreenCoordinate coordinate) {
+    if (trackingMode == TrackingMode.Display_and_tracking) {
+      trackingMode = TrackingMode.Display_only;
+    }
+    updateCamera();
+  }
+
+  _gpsButton() {
+    if (trackingMode == TrackingMode.Off) {
+      trackingMode = TrackingMode.Display_and_tracking;
+    } else {
+      trackingMode = TrackingMode.Off;
+    }
+    updateCamera();
+  }
+
+  // We need to implement our own location tracking. Basically we need 3 kinds of state and 1 button.
+  // State: 1.Display_location_and_camera_tracking / 2.Display_location_only / 3.Off.
+  // The button will toggle between 1/2 -> 3 or 3 -> 1.
+  // When user touched the map, then the state will stay as 3 or change from 1 to 2.
+  updateCamera() async {
+    if (trackingMode == TrackingMode.Display_and_tracking) {
+      await mapboxMap?.location
+          .updateSettings(LocationComponentSettings(enabled: true));
+      Position? position = await mapboxMap?.style.getPuckPosition();
+      await mapboxMap?.setCamera(CameraOptions(
+        center: Point(coordinates: position!).toJson(),
+      ));
+      trackTimer ??= Timer.periodic(const Duration(seconds: 1), (Timer _) {
+        updateCamera();
+      });
+      return;
+    }
+
+    if (trackingMode == TrackingMode.Display_only) {
+      await mapboxMap?.location
+          .updateSettings(LocationComponentSettings(enabled: true));
+    }
+
+    if (trackingMode == TrackingMode.Off) {
+      await mapboxMap?.location
+          .updateSettings(LocationComponentSettings(enabled: false));
+    }
+    trackTimer?.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
+    updateCamera();
     return Scaffold(
-        body: (MapWidget(
-      key: const ValueKey("mapWidget"),
-      onMapCreated: _onMapCreated,
-      onCameraChangeListener: _onCameraChangeListener,
-      styleUri: MapboxStyles.OUTDOORS,
-      cameraOptions: CameraOptions(
-          // TODO: According to this: https://github.com/mapbox/mapbox-maps-flutter/issues/248
-          // We need to implement our own location tracking. Basically we need 3 kinds of state and 1 button.
-          // State: 1.Display_location_and_camera_tracking / 2.Display_location_only / 3.Off.
-          // The button will toggle between 1/2 -> 3 or 3 -> 1.
-          // When user touched the map, then the state will stay as 3 or change from 3 to 2.
-          center: Point(coordinates: Position(-80.1263, 25.7845)).toJson(),
-          zoom: 12.0),
-    )));
+      body: (MapWidget(
+        key: const ValueKey("mapWidget"),
+        onMapCreated: _onMapCreated,
+        onCameraChangeListener: _onCameraChangeListener,
+        onScrollListener: _onMapScrollListener,
+        styleUri: MapboxStyles.OUTDOORS,
+        cameraOptions: CameraOptions(
+            zoom: 12.0),
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          trackingMode == TrackingMode.Off
+              ? Icons.gps_not_fixed
+              : Icons.gps_fixed,
+          color: Colors.blue,
+        ),
+        onPressed: () {
+          _gpsButton();
+        },
+      ),
+    );
   }
 }
