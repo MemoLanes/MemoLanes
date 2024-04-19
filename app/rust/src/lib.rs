ca8c9db7c5338f9fdc6ac5e8588207ad2c64590a
Klzy <klzy@vlabpro.com>
2024-04-19 20:46:17 +0800

    Export gpx and kml data (#67)

--- app/rust/src/lib.rs
+++ app/rust/src/lib.rs
@@ -11,6 +11,7 @@ mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be
 
 pub mod api;
 pub mod archive;
+pub mod export_data;
 pub mod gps_processor;
 pub mod import_data;
 pub mod journey_bitmap;
