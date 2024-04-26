41e682cf88a162cdac64d9e4876791310f9020d6
Zed(Zijun) Chen <zed@zijun.dev>
2024-04-26 20:19:21 +0800

    _

--- app/rust/src/cache_db.rs
+++ app/rust/src/cache_db.rs
@@ -1,19 +1,41 @@
 extern crate simplelog;
 use anyhow::Result;
-use rusqlite::{params, Connection, Error as RusqliteError};
+use rusqlite::{params, Connection, OptionalExtension};
 use std::path::Path;
-use std::io::Cursor;
 
-use crate::journey_data::deserialize_journey_bitmap;
-use crate::journey_bitmap::JourneyBitmap;
+use crate::journey_bitmap::{self, JourneyBitmap};
+use crate::journey_data::{self, JourneyData};
+use crate::journey_header::JourneyType;
 
 // Function to open the database and run migrations
 #[allow(clippy::type_complexity)]
 fn open_db(cache_dir: &str, file_name: &str, sql: &str) -> Result<Connection> {
+    // TODO: maybe we want versioning or at least detect versioning issue
+    // so we could rebuilt it.
     debug!("opening cache db for {}", file_name);
     let conn = Connection::open(Path::new(cache_dir).join(file_name))?;
     conn.execute(sql, [])?;
     Ok(conn)
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum JourneyCacheKey {
+    All,
+}
+
+impl JourneyCacheKey {
+    fn to_string(&self) -> String {
+        match self {
+            Self::All => "A".to_owned(),
+        }
+    }
+
+    fn of_string(str: &str) -> Self {
+        match str {
+            "A" => Self::All,
+            _ => panic!("Invalid `JourneyCacheKey`, str = {}", str),
+        }
+    }
 }
 
 // CacheDb structure
@@ -27,8 +49,8 @@ impl CacheDb {
         let conn = open_db(
             cache_dir,
             "cache.db",
-            "CREATE TABLE IF NOT EXISTS journey_cache (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+            "CREATE TABLE IF NOT EXISTS `journey_cache` (
+                        key TEXT PRIMARY KEY NOT NULL UNIQUE,
                         data BLOB NOT NULL
                     );",
         )
@@ -36,47 +58,64 @@ impl CacheDb {
         CacheDb { conn }
     }
 
-    // Method to flush the cache
     pub fn flush(&self) -> Result<()> {
         self.conn.cache_flush()?;
         Ok(())
     }
 
-    pub fn get_journey(&self) -> Result<Option<JourneyBitmap>> {
-        let query = "SELECT * FROM journey_cache ORDER BY id DESC LIMIT 1;";
-        let result = self.conn.query_row(query, params![], |row| {
-            let _journey_type = row.get_ref(0)?.as_i64()?;
-            let data = row.get_ref(1)?.as_blob()?;
-            Ok(data.to_vec())
-        });
+    fn get_journey_cache(&self, key: &JourneyCacheKey) -> Result<Option<JourneyBitmap>> {
+        let mut query = self
+            .conn
+            .prepare("SELECT data FROM `journey_cache` WHERE key = ?1;")?;
 
-        match result {
-            Ok(data) => {
-                let cursor = Cursor::new(data);
-                let journey_bitmap = deserialize_journey_bitmap(cursor);
-                match journey_bitmap {
-                    Ok(bitmap) => Ok(Some(bitmap)),
-                    Err(e) => Err(e.into())
-                }
-            },
-            Err(RusqliteError::QueryReturnedNoRows) => Ok(None),
-            Err(e) => Err(e.into()),
+        let data = query
+            .query_row([key.to_string()], |row| {
+                let data = row.get_ref(0)?.as_blob()?;
+                Ok(journey_data::deserialize_journey_bitmap(data))
+            })
+            .optional()?;
+
+        match data {
+            None => Ok(None),
+            Some(journey_bitmap) => Ok(Some(journey_bitmap?)),
         }
     }
 
-    // Method to insert journey bitmap blob
-    pub fn insert_journey_bitmap(&self, date_bytes: Vec<u8>) -> Result<()> {
-        let sql = "INSERT INTO journey_cache (data) VALUES (?1);";
-        self.conn.execute(sql, [&date_bytes])?;
-
+    fn set_journey_cache(
+        &self,
+        key: &JourneyCacheKey,
+        journey_bitmap: &JourneyBitmap,
+    ) -> Result<()> {
+        let mut data = Vec::new();
+        journey_data::serialize_journey_bitmap(journey_bitmap, &mut data)?;
+        self.conn.execute(
+            "INSERT OR REPLACE INTO `journey_cache` (key, data) VALUES (?1, ?2)",
+            (key.to_string(), &data),
+        )?;
         Ok(())
     }
 
-    pub fn delete_cached_journey(&self) -> Result<()> {
-        // TODO: in v1, use more fine-grained delete with year/month
-        let sql = "DELETE FROM journey_cache;";
-        self.conn.execute(sql, [])?;
+    pub fn get_journey_cache_or_compute<F>(
+        &self,
+        key: &JourneyCacheKey,
+        f: F,
+    ) -> Result<JourneyBitmap>
+    where
+        F: FnOnce() -> Result<JourneyBitmap>,
+    {
+        match self.get_journey_cache(key)? {
+            Some(journey_bitmap) => Ok(journey_bitmap),
+            None => {
+                let journey_bitmap = f()?;
+                self.set_journey_cache(key, &journey_bitmap)?;
+                Ok(journey_bitmap)
+            }
+        }
+    }
 
+    pub fn clear_journey_cache(&self) -> Result<()> {
+        // TODO: in v1, use more fine-grained delete with year/month
+        self.conn.execute("DELETE FROM journey_cache;", [])?;
         Ok(())
     }
 }
