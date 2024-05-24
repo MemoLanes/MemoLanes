1e4d4333775f38d96bcc0e3a3c4e755354ea5607
Zed(Zijun) Chen <zed@zijun.dev>
2024-05-24 20:18:45 +0800

    Fetch some basic info for debugging. (#87)

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:project_dv/archive.dart';
 import 'package:project_dv/gps_page.dart';
@@ -6,21 +9,53 @@ import 'package:project_dv/gps_recording_state.dart';
 import 'package:project_dv/journey.dart';
 import 'package:project_dv/map.dart';
 import 'package:project_dv/raw_data.dart';
-import 'package:project_dv/src/rust/api/api.dart';
+import 'package:project_dv/src/rust/api/api.dart' as api;
 import 'package:project_dv/src/rust/frb_generated.dart';
 import 'package:provider/provider.dart';
 
+void delayedInit() {
+  Future.delayed(const Duration(milliseconds: 2000), () async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    String? manufacturer;
+    String? model;
+    String? systemVersion;
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      var androidInfo = await deviceInfo.androidInfo;
+      manufacturer = androidInfo.manufacturer;
+      model = androidInfo.model;
+      systemVersion = androidInfo.version.release;
+    } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+      var iosInfo = await deviceInfo.iosInfo;
+      manufacturer = "Apple";
+      model = iosInfo.utsname.machine;
+      systemVersion = iosInfo.systemVersion;
+    }
+
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+    await api.delayedInit(
+        deviceInfo: api.DeviceInfo(
+            manufacturer: manufacturer,
+            model: model,
+            systemVersion: systemVersion),
+        appInfo: api.AppInfo(
+            packageName: packageInfo.packageName,
+            version: packageInfo.version,
+            buildNumber: packageInfo.buildNumber));
+  });
+}
 
 void main() async {
   // This is required since we are doing things before calling `runApp`.
   WidgetsFlutterBinding.ensureInitialized();
   // TODO: Consider using `flutter_native_splash`
   await RustLib.init();
-  await init(
+  await api.init(
       tempDir: (await getTemporaryDirectory()).path,
       docDir: (await getApplicationDocumentsDirectory()).path,
       supportDir: (await getApplicationSupportDirectory()).path,
       cacheDir: (await getApplicationCacheDirectory()).path);
+  delayedInit();
   runApp(
     MultiProvider(
       providers: [
