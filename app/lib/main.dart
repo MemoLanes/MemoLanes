715d839aeea9a03ce7bdbc32d1630c186c5c4aa6
lazycat <whitelion.b@outlook.com>
2024-07-10 14:05:47 +0800

    -

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -79,7 +79,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      onGene
       theme: ThemeData(
         // This is the theme of your application.
         //
