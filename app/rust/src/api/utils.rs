88080c2b6f61bf2ddf4ec98aac3cab158a04731a
Zed(Zijun) Chen <zed@zijun.dev>
2024-07-17 21:36:42 +0800

    _

--- app/rust/src/api/utils.rs
+++ app/rust/src/api/utils.rs
@@ -1,6 +1,11 @@
+pub use chrono::NaiveDate;
 use flutter_rust_bridge::frb;
 
 // TODO: frb does not support `chrono::NaiveDate`
+#[frb(opaque)]
+#[frb(mirror(NaiveDate))]
+pub struct _NaiveDate {}
+
 #[frb(sync)]
 pub fn naive_date_to_string(date: &chrono::NaiveDate) -> String {
     date.to_string()
