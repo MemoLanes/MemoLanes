d832e96981d6c7aa526dcb2669c8a43e1b49b823
Zed(Zijun) Chen <zed@zijun.dev>
2024-07-21 22:08:49 +0800

    _

--- app/rust/tests/gps_processor.rs
+++ app/rust/tests/gps_processor.rs
@@ -6,6 +6,7 @@ use std::collections::HashMap;
 #[test]
 fn first_data() {
     let mut gps_processor = GpsProcessor::new();
+    assert!(gps_processor.last_data().is_none());
     let data = RawData {
         latitude: 120.163856,
         longitude: 30.2719716,
@@ -14,11 +15,7 @@ fn first_data() {
         altitude: Some(10.),
         speed: Some(0.6028665),
     };
-    gps_processor.preprocess(data, |last_data, curr_data, result| {
-        assert!(last_data.is_none());
-        assert_eq!(curr_data.altitude.unwrap(), 10.);
-        assert_eq!(ProcessResult::NewSegment, result);
-    });
+    assert_eq!(gps_processor.preprocess(&data), ProcessResult::NewSegment);
 }
 
 #[test]
@@ -32,74 +29,54 @@ fn ignore() {
         altitude: Some(10.),
         speed: Some(0.6028665),
     };
-    gps_processor.preprocess(data, |_, _, result| {
-        assert_eq!(ProcessResult::Ignore, result);
-    });
+    assert_eq!(gps_processor.preprocess(&data), ProcessResult::Ignore);
 }
 
 #[test]
 fn time_difference() {
     let mut gps_processor = GpsProcessor::new();
 
-    gps_processor.preprocess(
-        RawData {
-            latitude: 120.163856,
-            longitude: 30.2719716,
-            timestamp_ms: Some(1697349116449),
-            accuracy: Some(3.9),
-            altitude: Some(10.),
-            speed: Some(0.6028665),
-        },
-        |_, _, _| {},
-    );
+    gps_processor.preprocess(&RawData {
+        latitude: 120.163856,
+        longitude: 30.2719716,
+        timestamp_ms: Some(1697349116449),
+        accuracy: Some(3.9),
+        altitude: Some(10.),
+        speed: Some(0.6028665),
+    });
 
-    gps_processor.preprocess(
-        RawData {
-            latitude: 120.1639266,
-            longitude: 30.271981,
-            timestamp_ms: Some(1697349117449),
-            accuracy: Some(3.5),
-            altitude: Some(20.),
-            speed: Some(0.18825254),
-        },
-        |last_data, curr_data, result| {
-            assert_eq!(last_data.as_ref().unwrap().altitude.unwrap(), 10.);
-            assert_eq!(curr_data.altitude.unwrap(), 20.);
-            assert_eq!(ProcessResult::Append, result);
-        },
-    );
+    assert_eq!(gps_processor.last_data().unwrap().altitude.unwrap(), 10.);
+    let result = gps_processor.preprocess(&RawData {
+        latitude: 120.1639266,
+        longitude: 30.271981,
+        timestamp_ms: Some(1697349117449),
+        accuracy: Some(3.5),
+        altitude: Some(20.),
+        speed: Some(0.18825254),
+    });
+    assert_eq!(ProcessResult::Append, result);
 
-    gps_processor.preprocess(
-        RawData {
-            latitude: 120.163856,
-            longitude: 30.2719716,
-            timestamp_ms: Some(1698349116449),
-            accuracy: Some(3.9),
-            altitude: Some(30.),
-            speed: Some(0.6028665),
-        },
-        |last_data, curr_data, result| {
-            assert_eq!(last_data.as_ref().unwrap().altitude.unwrap(), 20.);
-            assert_eq!(curr_data.altitude.unwrap(), 30.);
-            assert_eq!(ProcessResult::NewSegment, result);
-        },
-    );
+    assert_eq!(gps_processor.last_data().unwrap().altitude.unwrap(), 20.);
+    let result = gps_processor.preprocess(&RawData {
+        latitude: 120.163856,
+        longitude: 30.2719716,
+        timestamp_ms: Some(1698349116449),
+        accuracy: Some(3.9),
+        altitude: Some(30.),
+        speed: Some(0.6028665),
+    });
+    assert_eq!(ProcessResult::NewSegment, result);
 
-    gps_processor.preprocess(
-        RawData {
-            latitude: 120.163856,
-            longitude: 30.2719716,
-            timestamp_ms: Some(1697349116449),
-            accuracy: Some(3.9),
-            altitude: Some(10.),
-            speed: Some(0.6028665),
-        },
-        |last_data, curr_data, result| {
-            assert_eq!(last_data.as_ref().unwrap().altitude.unwrap(), 30.);
-            assert_eq!(curr_data.altitude.unwrap(), 10.);
-            assert_eq!(ProcessResult::Ignore, result);
-        },
-    );
+    assert_eq!(gps_processor.last_data().unwrap().altitude.unwrap(), 30.);
+    let result = gps_processor.preprocess(&RawData {
+        latitude: 120.163856,
+        longitude: 30.2719716,
+        timestamp_ms: Some(1697349116449),
+        accuracy: Some(3.9),
+        altitude: Some(10.),
+        speed: Some(0.6028665),
+    });
+    assert_eq!(ProcessResult::Ignore, result);
 }
 
 #[test]
@@ -107,9 +84,8 @@ fn run_though_test_data() {
     let mut gps_processor = GpsProcessor::new();
     let mut counter = HashMap::new();
     for data in test_utils::load_raw_gpx_data_for_test() {
-        gps_processor.preprocess(data, |_, _, result| {
-            counter.entry(result).and_modify(|c| *c += 1).or_insert(1);
-        });
+        let result = gps_processor.preprocess(&data);
+        counter.entry(result).and_modify(|c| *c += 1).or_insert(1);
     }
     assert_eq!(counter[&ProcessResult::NewSegment], 8);
     assert_eq!(counter[&ProcessResult::Append], 3577);
