b3e17875db54017aa2313d80007ae641401d024b
Zed(Zijun) Chen <zed@zijun.dev>
2024-07-27 23:55:30 +0800

    _

--- app/lib/gps_recording_state.dart
+++ app/lib/gps_recording_state.dart
@@ -194,21 +194,28 @@ class GpsRecordingState extends ChangeNotifier {
       }
     }
 
+    if (await Permission.location.isPermanentlyDenied ||
+        await Permission.notification.isPermanentlyDenied) {
+      await Geolocator.openAppSettings();
+      throw "Please allow location & notification permissions";
+    }
+
     if (!await Permission.notification.isGranted) {
-      await Permission.notification.request();
-      if (!await Permission.notification.isGranted) {
+      if (!await Permission.notification.request().isGranted) {
         throw "notification permission not granted";
       }
     }
 
-    LocationPermission permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.whileInUse) {
-      await Permission.locationAlways.request();
-      permission = await Geolocator.checkPermission();
+    if (!await Permission.location.isGranted) {
+      if (!await Permission.location.request().isGranted) {
+        throw "location permission not granted";
+      }
     }
-    if (permission != LocationPermission.always) {
-      await Geolocator.openAppSettings();
-      throw "Please allow location permissions";
+
+    if (!await Permission.locationAlways.isGranted) {
+      // It seems this does not wait for the result on iOS, and always
+      // permission is not strictly required.
+      await Permission.locationAlways.request();
     }
   }
 
