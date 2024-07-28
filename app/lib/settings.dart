66a4681ab1178f16f25e13485fa526c58be75c0b
Zed(Zijun) Chen <zed@zijun.dev>
2024-07-28 15:24:01 +0800

    _

--- app/lib/settings.dart
+++ app/lib/settings.dart
@@ -6,7 +6,6 @@ import 'package:memolanes/gps_recording_state.dart';
 import 'package:memolanes/utils.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:memolanes/src/rust/api/api.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
