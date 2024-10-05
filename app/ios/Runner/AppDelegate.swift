5dff6a582ae90420f5aa0b12ec93f5f135e42725
Zed(Zijun) Chen <zed@zijun.dev>
2024-10-05 18:22:36 +1000

    _

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import notification_when_app_is_killed
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -13,5 +14,10 @@ import Flutter
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func applicationWillTerminate(_ application: UIApplication) {
+    let notificationWhenAppIsKilledInstance = NotificationWhenAppIsKilledPlugin.instance
+    notificationWhenAppIsKilledInstance.applicationWillTerminate();
   }
 }
