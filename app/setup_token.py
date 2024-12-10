357bf01b70c0172930404567ce27fbae2b842fe4
Licheng Mao <tavimori@gmail.com>
2024-12-10 11:16:37 +0800

    _

--- app/setup_token.py
+++ app/setup_token.py
@@ -31,6 +31,20 @@ def generate_token_dart(token_content):
         file.write(dart_content)
     print(f"token.dart file generated at: {token_path}")
 
+def generate_json_token(token_content):
+    import json
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    token_path = os.path.join(script_dir, "journey_kernel/examples/token.json")
+    
+    # Create directory if it doesn't exist
+    os.makedirs(os.path.dirname(token_path), exist_ok=True)
+    
+    token_data = {"MAPBOX-ACCESS-TOKEN": token_content}
+    with open(token_path, "w") as file:
+        json.dump(token_data, file, indent=2)
+    print(f"token json file generated at: {token_path}")
+
+
 if __name__ == "__main__":
     data = {}
     env_path = ".env"
@@ -47,3 +61,4 @@ if __name__ == "__main__":
     generate_netrc("api.mapbox.com", "mapbox", data["MAPBOX-SDK-REGISTRY-TOKEN"])
     generate_gradle_token(data["MAPBOX-SDK-REGISTRY-TOKEN"])
     generate_token_dart(data["MAPBOX-ACCESS-TOKEN"])
+    generate_json_token(data["MAPBOX-ACCESS-TOKEN"])
