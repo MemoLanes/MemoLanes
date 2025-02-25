ab29357f792de5bf87d3a217b63a7ff3e1e5aaba
胡泽豪 <huzehao@huzehaodeMac-mini.local>
2025-02-25 20:56:36 +0800

    test

--- app/rust/src/merged_journey_builder.rs
+++ app/rust/src/merged_journey_builder.rs
@@ -13,7 +13,7 @@ use crate::{
 use anyhow::Result;
 use chrono::NaiveDate;
 
-//git test
+//git test2
 pub fn add_journey_vector_to_journey_bitmap(
     journey_bitmap: &mut JourneyBitmap,
     journey_vector: &JourneyVector,
