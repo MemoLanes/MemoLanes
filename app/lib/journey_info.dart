4112f3c58f9ae9124964edf7beec9c2aec3817e7
kuailezhiyuan <972833608@qq.com>
2025-04-06 22:23:17 +0800

    Add journey export and sharing features with localization updates

--- app/lib/journey_info.dart
+++ app/lib/journey_info.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:memolanes/component/base_map_webview.dart';
 import 'package:memolanes/journey_edit.dart';
 import 'package:memolanes/src/rust/api/api.dart' as api;
@@ -11,6 +12,8 @@ import 'package:memolanes/src/rust/journey_header.dart';
 import 'package:memolanes/utils.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
+
+enum ExportType { mldx, kml, gpx }
 
 class JourneyInfoPage extends StatefulWidget {
   const JourneyInfoPage({super.key, required this.journeyHeader});
@@ -37,26 +40,139 @@ class _JourneyInfoPage extends State<JourneyInfoPage> {
     });
   }
 
-  _export(JourneyHeader journeyHeader, api.ExportType exportType) async {
+  _saveData(JourneyInfo journeyInfo) async {
+    await api.updateJourneyMetadata(
+        id: widget.journeyHeader.id, journeyinfo: journeyInfo);
+  }
+
+  _deleteJourneyInfo() async {
+    if (await showCommonDialog(
+        context, context.tr("journey.delete_journey_message"),
+        hasCancel: true,
+        title: context.tr("journey.delete_journey_title"),
+        confirmText: context.tr("journey.delete"),
+        confirmGroundColor: Colors.red,
+        confirmTextColor: Colors.white)) {
+      await api.deleteJourney(journeyId: widget.journeyHeader.id);
+      if (!context.mounted) return;
+      Navigator.pop(context, true);
+    }
+  }
+
+  _editJourneyInfo() async {
+    final result =
+        await Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(context.tr("journey.journey_info_edit_bar_title")),
+        ),
+        body: Center(
+          child: JourneyInfoEditor(
+            startTime: widget.journeyHeader.start,
+            endTime: widget.journeyHeader.end,
+            journeyDate: widget.journeyHeader.journeyDate,
+            note: widget.journeyHeader.note,
+            journeyKind: widget.journeyHeader.journeyKind,
+            saveData: _saveData,
+          ),
+        ),
+      );
+    }));
+    if (result == true) {
+      // TODO: We should just refresh the page instead of closing it.
+      if (!context.mounted) return;
+      Navigator.pop(context, true);
+    }
+  }
+
+  Future<String> _saveFile(JourneyHeader journeyHeader, ExportType exportType) async {
     var tmpDir = await getTemporaryDirectory();
     var filepath =
         "${tmpDir.path}/${journeyHeader.revision}.${exportType.name}";
-    await api.exportJourney(
-        targetFilepath: filepath,
-        journeyId: journeyHeader.id,
-        exportType: exportType);
+    switch (exportType) {
+      case ExportType.mldx:
+        await api.generateSingleArchive(
+            journeyId: journeyHeader.id, targetFilepath: filepath);
+        break;
+      case ExportType.kml:
+        await api.exportJourney(
+            targetFilepath: filepath,
+            journeyId: journeyHeader.id,
+            exportType: api.ExportType.kml);
+        break;
+      case ExportType.gpx:
+        await api.exportJourney(
+            targetFilepath: filepath,
+            journeyId: journeyHeader.id,
+            exportType: api.ExportType.gpx);
+        break;
+    }
+    return filepath;
+  }
+
+  _share(JourneyHeader journeyHeader, ExportType exportType) async {
+    String filepath = await _saveFile(journeyHeader, exportType);
     await Share.shareXFiles([XFile(filepath)]);
     try {
       await File(filepath).delete();
     } catch (e) {
-      print(e);
-      // don't care about error
+      debugPrint('Failed to delete file: $e');
     }
   }
 
-  _saveData(JourneyInfo journeyInfo) async {
-    await api.updateJourneyMetadata(
-        id: widget.journeyHeader.id, journeyinfo: journeyInfo);
+  _showDialog(BuildContext context, JourneyHeader journeyHeader,
+      ExportType exportType) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(context.tr("journey.export_journey_data_title")),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: FaIcon(
+                      FontAwesomeIcons.floppyDisk,
+                      size: 40,
+                    ),
+                    onPressed: () async {
+                      String filepath = await _saveFile(journeyHeader, exportType);
+                      await showCommonDialog(
+                          context, filepath,
+                          title: context.tr("journey.save_journey_data_title"),
+                          confirmText: context.tr("common.ok"));
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  Text(context.tr("journey.save_journey_data_title")),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: FaIcon(
+                      FontAwesomeIcons.shareFromSquare,
+                      size: 40,
+                    ),
+                    onPressed: () {
+                      _share(journeyHeader, exportType);
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  Text(context.tr("journey.share_journey_data_title")),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -67,9 +183,46 @@ class _JourneyInfoPage extends State<JourneyInfoPage> {
       JourneyKind.flight => context.tr("journey_kind.flight"),
     };
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Journey Info"),
-      ),
+      appBar: AppBar(title: Text(context.tr("journey.journey_info_bar_title")), actions: [
+        PopupMenuButton<ExportType>(
+          onSelected: (value) {
+            if (Platform.isAndroid) {
+              _showDialog(context, widget.journeyHeader, value);
+            } else if (Platform.isIOS) {
+              _share(widget.journeyHeader, value);
+            }
+          },
+          itemBuilder: (BuildContext context) {
+            return [
+              PopupMenuItem<ExportType>(
+                value: ExportType.mldx,
+                child: Text(context.tr("journey.export_mldx_data_menu")),
+              ),
+              PopupMenuItem<ExportType>(
+                value: ExportType.gpx,
+                child: Text(context.tr("journey.export_gpx_data_menu")),
+              ),
+              PopupMenuItem<ExportType>(
+                value: ExportType.kml,
+                child: Text(context.tr("journey.export_kml_data_menu")),
+              ),
+            ];
+          },
+          icon: Icon(Icons.share),
+        ),
+        IconButton(
+          onPressed: () async {
+            _editJourneyInfo();
+          },
+          icon: Icon(Icons.edit),
+        ),
+        IconButton(
+          onPressed: () async {
+            _deleteJourneyInfo();
+          },
+          icon: Icon(Icons.delete),
+        ),
+      ]),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -86,88 +239,6 @@ class _JourneyInfoPage extends State<JourneyInfoPage> {
                 "Created At: ${fmt.format(widget.journeyHeader.createdAt.toLocal())}"),
             Text("Revision: ${widget.journeyHeader.revision}"),
             Text("Note: ${widget.journeyHeader.note}"),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              ElevatedButton(
-                onPressed: () async {
-                  var tmpDir = await getTemporaryDirectory();
-                  var filepath =
-                      "${tmpDir.path}/${widget.journeyHeader.revision}.mldx";
-                  await api.generateSingleArchive(
-                      journeyId: widget.journeyHeader.id,
-                      targetFilepath: filepath);
-                  await Share.shareXFiles([XFile(filepath)]);
-                  try {
-                    var file = File(filepath);
-                    await file.delete();
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-                child: const Text("Export MLDX"),
-              ),
-              ElevatedButton(
-                onPressed: widget.journeyHeader.journeyType ==
-                        JourneyType.vector
-                    ? () => _export(widget.journeyHeader, api.ExportType.kml)
-                    : null,
-                child: const Text("Export KML"),
-              ),
-            ]),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              ElevatedButton(
-                onPressed: widget.journeyHeader.journeyType ==
-                        JourneyType.vector
-                    ? () => _export(widget.journeyHeader, api.ExportType.gpx)
-                    : null,
-                child: const Text("Export GPX"),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  final result = await Navigator.push(context,
-                      MaterialPageRoute(builder: (context) {
-                    return Scaffold(
-                      appBar: AppBar(
-                        title: const Text("Edit journey Info"),
-                      ),
-                      body: Center(
-                        child: JourneyInfoEditor(
-                          startTime: widget.journeyHeader.start,
-                          endTime: widget.journeyHeader.end,
-                          journeyDate: widget.journeyHeader.journeyDate,
-                          note: widget.journeyHeader.note,
-                          journeyKind: widget.journeyHeader.journeyKind,
-                          saveData: _saveData,
-                        ),
-                      ),
-                    );
-                  }));
-                  if (result == true) {
-                    // TODO: We should just refresh the page instead of closing it.
-                    if (!context.mounted) return;
-                    Navigator.pop(context, true);
-                  }
-                },
-                child: const Text("Edit"),
-              ),
-            ]),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              ElevatedButton(
-                onPressed: () async {
-                  if (await showCommonDialog(
-                      context, context.tr("journey.delete_journey_message"),
-                      hasCancel: true,
-                      title: context.tr("journey.delete_journey_title"),
-                      confirmText: context.tr("journey.delete"),
-                      confirmGroundColor: Colors.red,
-                      confirmTextColor: Colors.white)) {
-                    await api.deleteJourney(journeyId: widget.journeyHeader.id);
-                    if (!context.mounted) return;
-                    Navigator.pop(context, true);
-                  }
-                },
-                child: const Text("Delete"),
-              ),
-            ]),
             Expanded(
               child: mapRendererProxy == null
                   ? (const CircularProgressIndicator())
