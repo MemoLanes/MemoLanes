c710a617e23537c6245e045b6b147acbbf78fd35
Zed(Zijun) Chen <zed@zijun.dev>
2025-05-11 20:54:26 +0800

    Language file tweak (#190)

--- app/lib/raw_data.dart
+++ app/lib/raw_data.dart
@@ -65,7 +65,7 @@ class _RawDataPage extends State<RawDataPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Raw Data Mode"),
+        title: Text(context.tr("advance_settings.raw_data_mode")),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
