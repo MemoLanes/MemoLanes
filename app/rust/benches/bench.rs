1bf9ae7abbceb09cc155fa276cd7fae80d29a001
RuriAsayuki <ruriasayuki@outlook.com>
2025-07-09 22:40:29 +0800

    High latitude width compensation (#203)
    Co-authored-by: Zed(Zijun) Chen <zed@zijun.dev>

--- app/rust/benches/bench.rs
+++ app/rust/benches/bench.rs
@@ -43,5 +43,40 @@ fn journey_area_calculation(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, journey_area_calculation);
+fn journey_bitmap(c: &mut Criterion) {
+    let mut group = c.benchmark_group("journey_bitmap");
+    group.sample_size(40);
+
+    group.bench_function("add_journey_vector_to_journey_bitmap", |b| {
+        let load_journey_vector = |name| {
+            let filename = format!("./tests/data/{name}.gpx");
+            let raw_data = import_data::load_gpx(&filename).unwrap();
+            import_data::journey_vector_from_raw_data(&raw_data, true).unwrap()
+        };
+
+        let nelson_to_wharariki_beach = load_journey_vector("nelson_to_wharariki_beach");
+        let heihe = load_journey_vector("raw_gps_heihe");
+
+        b.iter(|| {
+            let mut journey_bitmap = JourneyBitmap::new();
+            std::hint::black_box(
+                merged_journey_builder::add_journey_vector_to_journey_bitmap(
+                    &mut journey_bitmap,
+                    &nelson_to_wharariki_beach,
+                ),
+            );
+            std::hint::black_box(
+                merged_journey_builder::add_journey_vector_to_journey_bitmap(
+                    &mut journey_bitmap,
+                    &heihe,
+                ),
+            );
+            journey_bitmap
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, journey_area_calculation, journey_bitmap);
 criterion_main!(benches);
--- app/rust/tests/journey_bitmap.rs
+++ app/rust/tests/journey_bitmap.rs
@@ -208,6 +208,11 @@ fn vector_to_bitmap_laojunshan() {
 }
 
 #[test]
+fn vector_to_bitmap_heihe() {
+    vector_to_bitmap("heihe", 13, None);
+}
+
+#[test]
 fn vector_to_bitmap_nelson_to_wharariki_beach() {
     // `nelson_to_wharariki_beach.gps` is not a raw gps because it does not contian timestamps,
     // but it is good enough for this test.
@@ -228,4 +233,73 @@ fn draw_single_point() {
         journey_area_utils::compute_journey_bitmap_area(&journey_bitmap, None),
         68
     );
+}
+
+#[test]
+fn draw_line_in_different_latitude() {
+    // base in lat 0
+    let mut journey_bitmap = JourneyBitmap::new();
+    journey_bitmap.add_line(120.0, 0.0, 120.0, 1.0);
+    journey_bitmap.add_line(120.0, 0.0, 121.0, 0.0);
+    journey_bitmap.add_line(120.0, 0.0, 121.0, 1.0);
+    assert_eq!(
+        journey_area_utils::compute_journey_bitmap_area(&journey_bitmap, None),
+        3183812
+    );
+
+    // width is 2 , lat 60
+    let mut journey_bitmap2 = JourneyBitmap::new();
+    journey_bitmap2.add_line(120.0, 60.0, 120.0, 61.0);
+    journey_bitmap2.add_line(120.0, 60.0, 122.0, 60.0);
+    journey_bitmap2.add_line(120.0, 60.0, 122.0, 61.0);
+    let area2 = journey_area_utils::compute_journey_bitmap_area(&journey_bitmap2, None);
+    assert_eq!(area2 / 100000, 31);
+
+    // width is 3 , lat 70.5
+    let mut journey_bitmap3 = JourneyBitmap::new();
+    journey_bitmap3.add_line(120.0, 70.5, 120.0, 71.5);
+    journey_bitmap3.add_line(120.0, 70.5, 123.0, 70.5);
+    journey_bitmap3.add_line(120.0, 70.5, 123.0, 71.5);
+    let area3 = journey_area_utils::compute_journey_bitmap_area(&journey_bitmap3, None);
+    assert_eq!(area3 / 100000, 31);
+
+    // width is 3 , lat -70.5
+    let mut journey_bitmap4 = JourneyBitmap::new();
+    journey_bitmap4.add_line(120.0, -70.5, 120.0, -71.5);
+    journey_bitmap4.add_line(120.0, -70.5, 123.0, -70.5);
+    journey_bitmap4.add_line(120.0, -70.5, 123.0, -71.5);
+    let area3 = journey_area_utils::compute_journey_bitmap_area(&journey_bitmap4, None);
+    assert_eq!(area3 / 100000, 31);
+}
+
+#[test]
+fn draw_line_with_width2() {
+    let mut journey_bitmap = JourneyBitmap::new();
+
+    journey_bitmap.add_line(120.0, 60.0, 120.0, 60.005);
+    journey_bitmap.add_line(120.0, 60.005, 120.01, 60.01);
+    journey_bitmap.add_line(120.01, 60.01, 120.0, 60.0);
+    journey_bitmap.add_line(120.0, 60.0, 120.005, 60.0);
+
+    let map_renderer = MapRenderer::new(journey_bitmap);
+
+    let render_result =
+        test_utils::render_map_overlay(&map_renderer, 13, 120.0, 60.01, 120.01, 60.0);
+    test_utils::verify_image("draw_line_with_width2", &render_result.data);
+}
+
+#[test]
+fn draw_line_with_width3() {
+    let mut journey_bitmap = JourneyBitmap::new();
+
+    journey_bitmap.add_line(120.0, 70.0, 120.0, 70.005);
+    journey_bitmap.add_line(120.0, 70.005, 120.01, 70.01);
+    journey_bitmap.add_line(120.01, 70.01, 120.0, 70.0);
+    journey_bitmap.add_line(120.0, 70.0, 120.005, 70.0);
+
+    let map_renderer = MapRenderer::new(journey_bitmap);
+
+    let render_result =
+        test_utils::render_map_overlay(&map_renderer, 13, 120.0, 70.01, 120.01, 70.0);
+    test_utils::verify_image("draw_line_with_width3", &render_result.data);
 }
