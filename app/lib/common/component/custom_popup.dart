b966e680f4978eacc26cb7b3559cad1704d174e9
kuailezhiyuan <972833608@qq.com>
2025-10-31 22:01:55 +0800

    remove

--- app/lib/common/component/custom_popup.dart
+++ app/lib/common/component/custom_popup.dart
@@ -1,10 +1,6 @@
-import 'dart:math';
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-
-enum _ArrowDirection { top, bottom, left, right }
 
 enum PopupPosition { auto, top, bottom, left, right }
 
@@ -14,9 +10,7 @@ class CustomPopup extends StatefulWidget {
   final Widget child;
   final bool isLongPress;
   final Color? backgroundColor;
-  final Color? arrowColor;
   final Color? barrierColor;
-  final bool showArrow;
   final EdgeInsets contentPadding;
   final double? contentRadius;
   final BoxDecoration? contentDecoration;
@@ -24,8 +18,6 @@ class CustomPopup extends StatefulWidget {
   final VoidCallback? onAfterPopup;
   final bool rootNavigator;
   final PopupPosition position;
-
-  /// 水平/垂直偏移量
   final double? horizontalOffset;
   final double? verticalOffset;
 
@@ -39,10 +31,8 @@ class CustomPopup extends StatefulWidget {
     this.anchorKey,
     this.isLongPress = false,
     this.backgroundColor,
-    this.arrowColor,
-    this.showArrow = false,
     this.barrierColor,
-    this.contentPadding = const EdgeInsets.all(8),
+    this.contentPadding = const EdgeInsets.all(16),
     this.contentRadius,
     this.contentDecoration,
     this.onBeforePopup,
@@ -73,8 +63,6 @@ class CustomPopupState extends State<CustomPopup> {
       _PopupRoute(
         targetRect: offset & renderBox.paintBounds.size,
         backgroundColor: widget.backgroundColor,
-        arrowColor: widget.arrowColor,
-        showArrow: widget.showArrow,
         barriersColor: widget.barrierColor,
         contentPadding: widget.contentPadding,
         contentRadius: widget.contentRadius,
@@ -104,13 +92,7 @@ class CustomPopupState extends State<CustomPopup> {
 class _PopupContent extends StatelessWidget {
   final Widget child;
   final GlobalKey childKey;
-  final GlobalKey arrowKey;
-  final _ArrowDirection arrowDirection;
-  final double arrowHorizontal;
-  final double arrowVertical;
   final Color? backgroundColor;
-  final Color? arrowColor;
-  final bool showArrow;
   final EdgeInsets contentPadding;
   final double? contentRadius;
   final BoxDecoration? contentDecoration;
@@ -119,106 +101,32 @@ class _PopupContent extends StatelessWidget {
     Key? key,
     required this.child,
     required this.childKey,
-    required this.arrowKey,
-    required this.arrowHorizontal,
-    this.arrowVertical = 0,
-    required this.showArrow,
-    this.arrowDirection = _ArrowDirection.top,
     this.backgroundColor,
-    this.arrowColor,
-    this.contentRadius,
     required this.contentPadding,
+    this.contentRadius,
     this.contentDecoration,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          key: childKey,
-          padding: contentPadding,
-          margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10).copyWith(
-            top: arrowDirection == _ArrowDirection.bottom ? 0 : null,
-            bottom: arrowDirection == _ArrowDirection.top ? 0 : null,
-            left: arrowDirection == _ArrowDirection.right ? 0 : null,
-            right: arrowDirection == _ArrowDirection.left ? 0 : null,
-          ),
-          constraints: const BoxConstraints(minWidth: 50),
-          decoration: contentDecoration ??
-              BoxDecoration(
-                color: backgroundColor ?? Colors.white,
-                borderRadius: BorderRadius.circular(contentRadius ?? 10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    blurRadius: 10,
-                  ),
-                ],
+    return Container(
+      key: childKey,
+      padding: contentPadding,
+      constraints: const BoxConstraints(minWidth: 50),
+      decoration: contentDecoration ??
+          BoxDecoration(
+            color: backgroundColor ?? Colors.black,
+            borderRadius: BorderRadius.circular(contentRadius ?? 10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10,
               ),
-          child: child,
-        ),
-        if (showArrow)
-          Positioned(
-            // top: arrowDirection == _ArrowDirection.top ? 2 : null,
-            bottom: arrowDirection == _ArrowDirection.bottom ? 2 : null,
-            left: arrowDirection == _ArrowDirection.top ||
-                arrowDirection == _ArrowDirection.bottom
-                ? arrowHorizontal
-                : null,
-            top: arrowDirection == _ArrowDirection.left ||
-                arrowDirection == _ArrowDirection.right
-                ? arrowVertical
-                : null,
-            right: arrowDirection == _ArrowDirection.left ? 2 : null,
-            child: RotatedBox(
-              key: arrowKey,
-              quarterTurns: _getArrowQuarterTurns(arrowDirection),
-              child: CustomPaint(
-                size: const Size(16, 8),
-                painter: _TrianglePainter(color: arrowColor ?? Colors.white),
-              ),
-            ),
+            ],
           ),
-      ],
+      child: child,
     );
   }
-
-  int _getArrowQuarterTurns(_ArrowDirection direction) {
-    switch (direction) {
-      case _ArrowDirection.top:
-        return 2;
-      case _ArrowDirection.bottom:
-        return 4;
-      case _ArrowDirection.left:
-      case _ArrowDirection.right:
-        return 1; // 左右箭头旋转90°
-    }
-  }
-}
-
-class _TrianglePainter extends CustomPainter {
-  final Color color;
-
-  const _TrianglePainter({required this.color});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint();
-    final path = Path();
-    paint.isAntiAlias = true;
-    paint.color = color;
-
-    path.moveTo(0, 0);
-    path.lineTo(size.width / 2, size.height);
-    path.lineTo(size.width, 0);
-    path.close();
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
 
 class _PopupRoute extends PopupRoute<void> {
@@ -229,27 +137,18 @@ class _PopupRoute extends PopupRoute<void> {
   final double? verticalOffset;
 
   final GlobalKey _childKey = GlobalKey();
-  final GlobalKey _arrowKey = GlobalKey();
   final Color? backgroundColor;
-  final Color? arrowColor;
-  final bool showArrow;
   final Color? barriersColor;
   final EdgeInsets contentPadding;
   final double? contentRadius;
   final BoxDecoration? contentDecoration;
 
-  static const double _margin = 10;
-
-  double _maxHeight = 0;
-  _ArrowDirection _arrowDirection = _ArrowDirection.top;
-  double _arrowHorizontal = 0;
-  double _arrowVertical = 0;
-  double _scaleAlignDx = 0.5;
-  double _scaleAlignDy = 0.5;
   double? _top;
   double? _bottom;
   double? _left;
   double? _right;
+  double _scaleAlignDx = 0.5;
+  double _scaleAlignDy = 0.5;
 
   final Duration animationDuration;
   final Curve animationCurve;
@@ -261,8 +160,6 @@ class _PopupRoute extends PopupRoute<void> {
     this.horizontalOffset,
     this.verticalOffset,
     this.backgroundColor,
-    this.arrowColor,
-    required this.showArrow,
     this.barriersColor,
     required this.contentPadding,
     this.contentRadius,
@@ -306,60 +203,33 @@ class _PopupRoute extends PopupRoute<void> {
       case PopupPosition.top:
         _top = null;
         _bottom = screenSize.height - targetRect.top + (verticalOffset ?? 0);
-        _arrowDirection = _ArrowDirection.bottom;
         _scaleAlignDy = 1;
         _left = targetRect.center.dx - childRect.width / 2 + (horizontalOffset ?? 0);
-        _right = null;
-        _arrowHorizontal = childRect.width / 2 - 8;
         break;
       case PopupPosition.bottom:
         _top = targetRect.bottom + (verticalOffset ?? 0);
-        _bottom = null;
-        _arrowDirection = _ArrowDirection.top;
         _scaleAlignDy = 0;
         _left = targetRect.center.dx - childRect.width / 2 + (horizontalOffset ?? 0);
-        _right = null;
-        _arrowHorizontal = childRect.width / 2 - 8;
         break;
       case PopupPosition.left:
         _left = targetRect.left - childRect.width + (horizontalOffset ?? 0);
-        _right = null;
-
-        // 垂直居中
         _top = targetRect.center.dy - childRect.height / 2 + (verticalOffset ?? 0);
-        _bottom = null;
-
-        _arrowDirection = _ArrowDirection.right;
-        _arrowVertical = childRect.height / 2 - 4; // 箭头垂直居中
+        _scaleAlignDx = 1;
         break;
-
       case PopupPosition.right:
         _left = targetRect.right + (horizontalOffset ?? 0);
-        _right = null;
-
-        // 垂直居中
         _top = targetRect.center.dy - childRect.height / 2 + (verticalOffset ?? 0);
-        _bottom = null;
-
-        _arrowDirection = _ArrowDirection.left;
-        _arrowVertical = childRect.height / 2 - 4; // 箭头垂直居中
+        _scaleAlignDx = 0;
         break;
-
       case PopupPosition.auto:
         if (screenSize.height - targetRect.bottom > targetRect.top) {
-          // 下方空间大
           _top = targetRect.bottom + (verticalOffset ?? 0);
-          _bottom = null;
-          _arrowDirection = _ArrowDirection.top;
-          _arrowHorizontal = childRect.width / 2 - 8;
+          _scaleAlignDy = 0;
         } else {
-          _top = null;
           _bottom = screenSize.height - targetRect.top + (verticalOffset ?? 0);
-          _arrowDirection = _ArrowDirection.bottom;
-          _arrowHorizontal = childRect.width / 2 - 8;
+          _scaleAlignDy = 1;
         }
         _left = targetRect.center.dx - childRect.width / 2 + (horizontalOffset ?? 0);
-        _right = null;
         break;
     }
   }
@@ -375,13 +245,7 @@ class _PopupRoute extends PopupRoute<void> {
       Animation<double> secondaryAnimation, Widget child) {
     child = _PopupContent(
       childKey: _childKey,
-      arrowKey: _arrowKey,
-      arrowHorizontal: _arrowHorizontal,
-      arrowVertical: _arrowVertical,
-      arrowDirection: _arrowDirection,
       backgroundColor: backgroundColor,
-      arrowColor: arrowColor,
-      showArrow: showArrow,
       contentPadding: contentPadding,
       contentRadius: contentRadius,
       contentDecoration: contentDecoration,
@@ -398,7 +262,7 @@ class _PopupRoute extends PopupRoute<void> {
           top: _top,
           bottom: _bottom,
           child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 300, maxHeight: 300),
+            constraints: const BoxConstraints(maxWidth: 300, maxHeight: 300),
             child: FadeTransition(
               opacity: curvedAnimation,
               child: ScaleTransition(
