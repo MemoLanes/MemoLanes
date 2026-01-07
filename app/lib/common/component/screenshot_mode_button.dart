08fd75b84db85b40ab6029342943cda1107817d7
kuailezhiyuan <972833608@qq.com>
2026-01-07 19:26:03 +0800

    _

--- app/lib/common/component/screenshot_mode_button.dart
+++ app/lib/common/component/screenshot_mode_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:memolanes/constants/style_constants.dart';
 import 'package:provider/provider.dart';
 
-// 假设 MultiProvider 中已经提供了 ValueNotifier<bool> screenshotMode
 class ScreenshotButton extends StatelessWidget {
 
   const ScreenshotButton({
@@ -13,7 +12,6 @@ class ScreenshotButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenshot = context.watch<ValueNotifier<bool>>();
 
-    // 截图模式开启时隐藏按钮
     if (screenshot.value) return const SizedBox.shrink();
 
     return SafeArea(
@@ -29,7 +27,7 @@ class ScreenshotButton extends StatelessWidget {
           ),
           child: IconButton(
             onPressed: () {
-              screenshot.value = true; // 点击开启截图模式
+              screenshot.value = true;
             },
             icon: const Icon(
               Icons.camera_alt,
