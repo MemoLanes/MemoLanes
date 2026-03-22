2da47a33d8971e463adc4ae26a15eb8f493a69d3
kuailezhiyuan <972833608@qq.com>
2026-03-22 22:03:07 +0800

    _

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Flutter
 import notification_when_app_is_killed
+import AppIntents
+import intelligence
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -19,5 +21,59 @@ import notification_when_app_is_killed
   override func applicationWillTerminate(_ application: UIApplication) {
     let notificationWhenAppIsKilledInstance = NotificationWhenAppIsKilledPlugin.instance
     notificationWhenAppIsKilledInstance.applicationWillTerminate();
+  }
+}
+
+@available(iOS 16.0, *)
+struct StartRecordingIntent: AppIntent {
+  static var title: LocalizedStringResource = "开始记录"
+  static var openAppWhenRun: Bool = false
+
+  @MainActor
+  func perform() async throws -> some IntentResult {
+    IntelligencePlugin.notifier.push("start_recording")
+    return .result()
+  }
+}
+
+@available(iOS 16.0, *)
+struct StopRecordingIntent: AppIntent {
+  static var title: LocalizedStringResource = "停止记录"
+  static var openAppWhenRun: Bool = false
+
+  @MainActor
+  func perform() async throws -> some IntentResult {
+    IntelligencePlugin.notifier.push("stop_recording")
+    return .result()
+  }
+}
+
+@available(iOS 16.0, *)
+struct MemoLanesAppShortcuts: AppShortcutsProvider {
+  static var appShortcuts: [AppShortcut] {
+    AppShortcut(
+      intent: StartRecordingIntent(),
+      phrases: [
+        "开始记录 \(.applicationName)",
+        "开始轨迹记录 \(.applicationName)",
+        "开始行程记录 \(.applicationName)",
+        "在 \(.applicationName) 开始记录",
+        "Start recording in \(.applicationName)",
+        "Start trip recording in \(.applicationName)"
+      ],
+      shortTitle: "开始记录"
+    )
+    AppShortcut(
+      intent: StopRecordingIntent(),
+      phrases: [
+        "停止记录 \(.applicationName)",
+        "停止轨迹记录 \(.applicationName)",
+        "结束行程记录 \(.applicationName)",
+        "在 \(.applicationName) 停止记录",
+        "Stop recording in \(.applicationName)",
+        "Stop trip recording in \(.applicationName)"
+      ],
+      shortTitle: "停止记录"
+    )
   }
 }
---
+++ app/lib/common/shortcut_handler_util.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:intelligence/intelligence.dart';
+import 'package:memolanes/common/gps_manager.dart';
+import 'package:memolanes/common/log.dart';
+
+class ShortcutHandlerUtil {
+  ShortcutHandlerUtil._();
+
+  static StreamSubscription<String>? _selectionSub;
+
+  static void init({required GpsManager gpsManager}) {
+    _selectionSub?.cancel();
+    _selectionSub = Intelligence().selectionsStream().listen(
+      (selection) async {
+        try {
+          switch (selection) {
+            case 'start_recording':
+              await gpsManager.changeRecordingState(GpsRecordingStatus.recording);
+              break;
+            case 'stop_recording':
+              await gpsManager.changeRecordingState(GpsRecordingStatus.none);
+              break;
+            default:
+              log.warning('Unknown shortcut selection: $selection');
+          }
+        } catch (e, s) {
+          log.error('Failed to handle shortcut selection: $selection', s);
+        }
+      },
+      onError: (Object err) {
+        log.error('Error in shortcut selection stream: $err');
+      },
+    );
+  }
+}
