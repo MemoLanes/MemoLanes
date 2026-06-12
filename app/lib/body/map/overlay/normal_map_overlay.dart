6f46d38ee49391d33a4289c9a450cf0e9af1980f
kuailezhiyuan <972833608@qq.com>
2026-06-12 22:53:29 +0800

    _

--- app/lib/body/map/overlay/normal_map_overlay.dart
+++ app/lib/body/map/overlay/normal_map_overlay.dart
@@ -6,6 +6,7 @@ import 'package:memolanes/common/component/map_controls/tracking_button.dart';
 import 'package:memolanes/common/component/rec_indicator.dart';
 import 'package:memolanes/common/component/recording_buttons.dart';
 import 'package:memolanes/common/gps_manager.dart';
+import 'package:memolanes/constants/style_constants.dart';
 import 'package:pointer_interceptor/pointer_interceptor.dart';
 import 'package:provider/provider.dart';
 
@@ -19,48 +20,85 @@ class NormalMapOverlay extends StatelessWidget {
   final TrackingMode trackingMode;
   final VoidCallback onTrackingPressed;
 
+  Widget _buildMapControls() {
+    return PointerInterceptor(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          TrackingButton(
+            trackingMode: trackingMode,
+            onPressed: onTrackingPressed,
+          ),
+          const AccuracyDisplay(),
+          LayerButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPortraitLayout(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const Spacer(),
+            Padding(
+              padding: EdgeInsets.only(
+                right: 8,
+                bottom: screenSize.height * 0.08,
+              ),
+              child: _buildMapControls(),
+            ),
+            const RecordingButtons(),
+            const SizedBox(height: 116),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLandscapeLayout(BuildContext context) {
+    final padding = MediaQuery.paddingOf(context);
+    final bottomInset = padding.bottom + StyleConstants.navBarSafeArea + 16.0;
+
+    return Stack(
+      children: [
+        Positioned(
+          right: padding.right + 32.0,
+          bottom: bottomInset,
+          child: _buildMapControls(),
+        ),
+        Positioned(
+          left: padding.left + 24.0,
+          right: padding.right + 24.0,
+          bottom: bottomInset,
+          child: const Align(
+            heightFactor: 1,
+            alignment: Alignment.center,
+            child: RecordingButtons(),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
     final isLandscape =
         MediaQuery.of(context).orientation == Orientation.landscape;
     final gpsManager = context.watch<GpsManager>();
 
     return Stack(
       children: [
-        SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Spacer(),
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: 8,
-                    bottom: isLandscape ? 16 : screenSize.height * 0.08,
-                  ),
-                  child: PointerInterceptor(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        TrackingButton(
-                          trackingMode: trackingMode,
-                          onPressed: onTrackingPressed,
-                        ),
-                        const AccuracyDisplay(),
-                        LayerButton(),
-                      ],
-                    ),
-                  ),
-                ),
-                const RecordingButtons(),
-                const SizedBox(height: 116),
-              ],
-            ),
-          ),
-        ),
+        if (isLandscape)
+          _buildLandscapeLayout(context)
+        else
+          _buildPortraitLayout(context),
         RecIndicator(
           isRecording:
               gpsManager.recordingStatus == GpsRecordingStatus.recording,
